2f328879ec81cdf0827bcaef851527b91e30d323
den0206 <mac.pop@mineo.jp>
2020-12-14 22:24:44 +0900

    Initial Commit

---
+++ Twi-Chat/Twi_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twi_ChatApp.swift
+//  Twi-Chat
+//
+//  Created by 酒井ゆうき on 2020/12/14.
+//
+
+import SwiftUI
+
+@main
+struct Twi_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
