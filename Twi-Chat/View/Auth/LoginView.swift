528621505e22f036818f7e14d303b7eb7ddd7045
den0206 <mac.pop@mineo.jp>
2020-12-25 23:34:10 +0900

    image picker

--- Twi-Chat/View/Auth/LoginView.swift
+++ Twi-Chat/View/Auth/LoginView.swift
@@ -13,64 +13,75 @@ struct LoginView: View {
     @State private var password : String = ""
     
     var body: some View {
-        VStack {
-            
-            Image("twitter-logo")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 220, height: 100)
-                .padding(.top, 88)
-                .padding(.bottom,32)
-            
-            VStack(spacing : 16) {
+        
+        NavigationView {
+            VStack {
                 
-                CustomTextField(text: $email, placeholder: "Email", imageName: "envelope")
+                Image("twitter-logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 220, height: 100)
+                    .padding(.top, 88)
+                    .padding(.bottom,32)
                 
-                CustomTextField(text: $password, placeholder: "Password", imageName: "lock",isSecure: true)
+                VStack(spacing : 16) {
+                    
+                    CustomTextField(text: $email, placeholder: "Email", imageName: "envelope")
+                    
+                    CustomTextField(text: $password, placeholder: "Password", imageName: "lock",isSecure: true)
+                    
+                }
                 
-            }
-            
-            HStack {
-                Spacer()
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {}) {
+                        Text("Forgot Your Password?")
+                            .font(.footnote)
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(.top,16)
+                            .padding(.trailing, 32)
+                    }
+                }
                 
                 Button(action: {}) {
-                    Text("Forgot Your Password?")
-                        .font(.footnote)
-                        .bold()
-                        .foregroundColor(.white)
-                        .padding(.top,16)
-                        .padding(.trailing, 32)
+                    Text("Sign In")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                        .frame(width: 360, height: 50)
+                        .background(Color.white)
+                        .cornerRadius(25)
+                        .clipShape(Capsule())
+                        .padding()
                 }
-            }
-            
-            Button(action: {}) {
-                Text("Sign In")
-                    .font(.headline)
-                    .foregroundColor(.blue)
-                    .frame(width: 360, height: 50)
-                    .background(Color.white)
-                    .cornerRadius(25)
-                    .clipShape(Capsule())
-                    .padding()
-            }
 
-            
-            Spacer()
-            
-            HStack {
-                Text("Don't Have an Account")
-                    .font(.system(size: 14))
                 
-                Text("Sign Up")
-                    .font(.system(size: 12,weight : .semibold))
+                Spacer()
+                
+                
+                NavigationLink(destination: RegistrationView().navigationBarBackButtonHidden(true)) {
+                    HStack {
+                        Text("Don't Have an Account")
+                            .font(.system(size: 14))
+                        
+                        
+                        Text("Sign Up")
+                            .font(.system(size: 12,weight : .semibold))
+                    }
+                    .foregroundColor(.white)
+                    .padding(.bottom,40)
+                }
+              
+                
+                Spacer()
             }
-            .foregroundColor(.white)
-            .padding(.bottom,40)
+            .background(Color(#colorLiteral(red: 0.1198849156, green: 0.6302657127, blue: 0.951220572, alpha: 1)))
+            .ignoresSafeArea(.all)
             
-            Spacer()
+            .navigationBarHidden(true)
         }
-        .background(Color(#colorLiteral(red: 0.1198849156, green: 0.6302657127, blue: 0.951220572, alpha: 1)))
-        .ignoresSafeArea(.all)
+      
     }
 }
 
--- Twi-Chat/View/Auth/RegistrationView.swift
+++ Twi-Chat/View/Auth/RegistrationView.swift
@@ -8,8 +8,97 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var selectedImage : Image?
+    
+    @State private var email : String = ""
+    @State private var password : String = ""
+    @State private var fullname : String = ""
+    @State private var username : String = ""
+    
+    @State private var showPicker = false
+  
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            
+            Button(action: {showPicker.toggle()}) {
+                
+                if selectedImage != nil {
+                    selectedImage!
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 140, height: 140)
+                        .clipped()
+                        .clipShape(Circle())
+                       
+                    
+                } else {
+                    Image("plus_photo")
+                        .resizable()
+                        .renderingMode(.template)
+                        .scaledToFill()
+                        .frame(width: 140, height: 140)
+                        .foregroundColor(.white)
+                }
+                
+            }
+            .padding(.top, 88)
+            .padding(.bottom,16)
+            .sheet(isPresented: $showPicker, content: {
+                ImagePicker(image: $selectedImage)
+            })
+            
+            
+            
+            VStack(spacing : 16) {
+                
+                CustomTextField(text: $email, placeholder: "Email", imageName: "envelope")
+                
+                CustomTextField(text: $password, placeholder: "Password", imageName: "lock",isSecure: true)
+                
+                CustomTextField(text: $fullname, placeholder: "Fullname", imageName: "person",isSecure: false)
+
+                CustomTextField(text: $username, placeholder: "UserName", imageName: "person",isSecure: false)
+
+                
+            }
+            
+            Button(action: {}) {
+                Text("Sign Up")
+                    .font(.headline)
+                    .foregroundColor(.blue)
+                    .frame(width: 360, height: 50)
+                    .background(Color.white)
+                    .cornerRadius(25)
+                    .clipShape(Capsule())
+                    .padding()
+            }
+
+            
+            Spacer()
+
+            Button(action: {presentationMode.wrappedValue.dismiss()}) {
+                HStack {
+                    Text("Already Have an Account")
+                        .font(.system(size: 14))
+                    
+                    Text("Sign In")
+                        .font(.system(size: 12,weight : .semibold))
+                }
+                .foregroundColor(.white)
+                .padding(.bottom,40)
+            }
+            
+            
+            Spacer()
+        }
+        .background(Color(#colorLiteral(red: 0.1198849156, green: 0.6302657127, blue: 0.951220572, alpha: 1)))
+        .ignoresSafeArea(.all)
     }
 }
 
@@ -17,4 +106,51 @@ struct RegistrationView_Previews: PreviewProvider {
     static var previews: some View {
         RegistrationView()
     }
+}
+
+//MARK: - Image Picker
+
+struct ImagePicker : UIViewControllerRepresentable {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image : Image?
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+  
+}
+
+extension ImagePicker {
+    
+    class Coordinator : NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent : ImagePicker
+        
+        init(_ parent : ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            guard let image = info[.originalImage] as? UIImage else {return}
+            parent.image = Image(uiImage: image)
+            
+            parent.presentationMode.wrappedValue.dismiss()
+
+        }
+    }
+    
+    
 }
